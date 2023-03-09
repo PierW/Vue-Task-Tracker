6359c9c6c8c96dd59906a4c7a40ba51ccf9b6d4c
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2023-03-09 11:52:05 +0100

    completato header e create rotte

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,8 +2,55 @@
 </script>
 
 <template>
-    <h1>Header</h1>
+    <header>
+        <nav class="container">
+            <router-link to="/" class="branding" title="vai alla home">
+                <img src="../assets/Vue_Logo_Black.png" alt="logo">
+                <h2>Vue Task Tracker</h2>
+            </router-link>
+            <ul class="nav-routes">
+                <li>
+                    <RouterLink to="/">Home</RouterLink>
+                </li>
+                <li>
+                    <router-link :to="{name : 'about'}">About</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <style lang="scss" scoped>
+header {
+  background-color: #f1f1f1;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      text-decoration: none;
+      color: #000;
+      img {
+        max-width: 50px;
+      }
+      h1 {
+        font-size: 24px;
+      }
+    }
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,21 @@
 
 <template>
   <main>
-
+    Home
   </main>
 </template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
